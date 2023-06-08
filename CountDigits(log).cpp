12c09ce6a9f16e6651bffc3701196a98be1bd9f8
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-06-08 12:28:07 +0530

    Create CountDigits(log).cpp

---
+++ CountDigits(log).cpp
@@ -0,0 +1,14 @@
+//count the number of digits in an integer.
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int input;
+	cin>>input;
+	int digit = floor(log10(input)+1);
+		
+	cout<<digit;	
+}
+
+
+
