24b9f38a7ceea80ef3e0ac85f04327584c3c7ef4
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-06-09 12:51:44 +0530

    Create CheckPallindrome.cpp

---
+++ CheckPallindrome.cpp
@@ -0,0 +1,22 @@
+//check if a number is a pallindroma and return true or false
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isPalindrome(int x) {
+        if (x < 0) return false;
+        long r=0;
+        int y=x;
+        while(y!=0){
+            r=r*10+y%10;
+            y=y/10;
+        }
+        return r==x;
+    }
+    
+int main(){
+    int x=0;
+    cin>>x;
+    cout<<isPalindrome(x);
+    
+}
+
