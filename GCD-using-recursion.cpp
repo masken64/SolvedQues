5e56da266c6cd344f2062b50ba7f500f4e1ae424
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-06-09 14:44:27 +0530

    Create GCD-using-recursion.cpp

---
+++ GCD-using-recursion.cpp
@@ -0,0 +1,14 @@
+//optimised recursive approach 
+
+#include<bits/stdc++.h>
+using namespace std;
+int gcd(int a, int b){
+    if(b==0) return a;
+    return gcd(b,a%b);
+    
+}
+int main(){
+    int a; int b;
+    cin>>a>>b;
+    cout<<gcd(a,b);
+}
