3f1942d072279205fdfd570cd4a4e32a0adaf0e2
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-06-10 17:45:33 +0530

    Create FactorialOfN(recursion).cpp

---
+++ FactorialOfN(recursion).cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+void n(int a, int sum){
+     
+     if(a<1) {
+         
+         cout<<sum; return;
+         
+     }
+     
+     n(a-1,sum*a);
+}
+int main(){
+    int a;
+    
+    cin>>a;
+    n(a,1);
+
+}
