6ee452c1b9289ad622f6a8cf7c5837947af5d529
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-06-13 19:15:15 +0530

    Create SumofFib.cpp

---
+++ SumofFib.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int fib(int n){
+    if(n<=1){
+        return n;
+    }
+    
+    int last = fib(n-1);
+    int slast = fib(n-2);
+    
+    return last + slast;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<fib(n)<<" ";
+}
