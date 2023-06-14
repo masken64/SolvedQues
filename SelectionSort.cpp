e46c2ce081f67b8ec3c338b5cad012b6df13692f
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-06-14 18:47:59 +0530

    Create SelectionSort.cpp

---
+++ SelectionSort.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+void
+sort (int a[], int n)
+{
+  for (int i = 0; i < n - 1; i++)
+    {
+      int mini = i;
+      for (int j = i + 1; j < n; j++)
+	{
+	  if (a[j] < a[mini])
+	    {
+	      mini = j;
+	    }
+	}
+      int temp = a[mini];
+      a[mini] = a[i];
+      a[i] = temp;
+    }
+  for (int i = 0; i < n; i++)
+    {
+      cout << a[i] << " ";
+    }
+  cout << endl;
+}
+
+int
+main ()
+{
+  int a[] = { 3, 2, 6, 8, 1, 0 };
+  int n = sizeof (a) / sizeof (a[0]);
+  for (int i = 0; i < n; i++)
+    {
+      cout << a[i] << " ";
+    }
+  cout << endl;
+  sort (a, n);
+}
