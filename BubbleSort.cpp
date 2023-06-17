b242c82e62e919af9a003c6266ab6296e34493fb
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-06-17 17:26:31 +0530

    Update BubbleSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -1,38 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
-void
-bubble (int a[], int n)
-{
-  for (int i = n - 1; i >= 0; i--)
-    {
-      for (int j = 0; j <= i - 1; j++)
-	{
-	  if (a[j] > a[j + 1])
-	    {
-		    // can also use swap(a[j],a[j+1]);
-	      int temp = a[j + 1];
-	      a[j + 1] = a[j];
-	      a[j] = temp;
-	    }
-	}
+void bubbleSort(int a[], int n){
+    for(int i = n-1; i>=0; i--){
+        for(int j =0; j<=i-1;j++){
+            if(a[j]>a[j+1]){
+                int temp = a[j];
+                a[j] = a[j+1];
+                a[j+1]= temp;
+            }
+        }
     }
-
-  for (int i = 0; i < n; i++)
-    {
-      cout << a[i] << " ";
+    cout<<"Sorted Array - "<<endl;
+    for(int i =0;i<n;i++){
+        cout<<a[i]<<" ";
     }
-    cout<<endl;
 }
-
-int
-main ()
-{
-  int a[] = { 5, 2, 1, 7, 4 };
-  int n = sizeof (a) / sizeof (a[0]);
-  for (int i = 0; i < n; i++)
-    {
-      cout << a[i] << " ";
+int main(){
+    int a[5];
+    cout<<"Enter Array"<<endl;
+    for(int i =0; i<5;i++){
+        cin>>a[i];
+    }
+    cout<<"Your array before using bubble sort - "<<endl;
+    int n = sizeof(a)/sizeof(a[0]);
+    for(int i =0; i<n;i++){
+        cout<<a[i]<<" ";
     }
     cout<<endl;
-  bubble (a, n);
+    bubbleSort(a,n);
 }
