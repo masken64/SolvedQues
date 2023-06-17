ba5514150c6f416c4d5d1643ce7b6150885f9648
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-06-17 18:26:25 +0530

    Update SelectionSort.cpp

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -1,39 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
-void
-sort (int a[], int n)
-{
-  for (int i = 0; i < n - 1; i++)
-    {
-      int mini = i;
-      for (int j = i + 1; j < n; j++)
-	{
-	  if (a[j] < a[mini])
-	    {
-	      mini = j;
-	    }
-	}
-	    //can also use swap(a[mini], a[i]);
-      int temp = a[mini];
-      a[mini] = a[i];
-      a[i] = temp;
-    }
-  for (int i = 0; i < n; i++)
-    {
-      cout << a[i] << " ";
-    }
-  cout << endl;
-}
 
-int
-main ()
-{
-  int a[] = { 3, 2, 6, 8, 1, 0 };
-  int n = sizeof (a) / sizeof (a[0]);
-  for (int i = 0; i < n; i++)
-    {
-      cout << a[i] << " ";
+void insertionSort(int a[], int n){
+    for(int i =0; i<n-1;i++){
+        int mini = i;
+        for(int j =i+1; j<n;j++){
+            if(a[j]<a[mini]){
+                mini =j;
+            }
+        }
+        swap(a[i],a[mini]);
     }
-  cout << endl;
-  sort (a, n);
+    cout<<"Sorted Array - "<<endl;
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+    
+}
+int main(){
+    int a[] = {5,4,3,2,1,0};
+    int n = sizeof(a)/sizeof(a[0]);
+    cout<<"Unsorted array "<<endl;
+    for(int i =0; i<n;i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+    insertionSort(a,n);
 }
