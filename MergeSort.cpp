0cff4d3741f56d7128fc9f036f7e8c74f75d5a46
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-06-18 18:39:39 +0530

    Update MergeSort.cpp

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -1,66 +1,30 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-void merge(vector<int> &arr, int low, int mid, int high) {
-    vector<int> temp; // temporary array
-    int left = low;      // starting index of left half of arr
-    int right = mid + 1;   // starting index of right half of arr
 
-    //storing elements in the temporary array in a sorted manner//
-
-    while (left <= mid && right <= high) {
-        if (arr[left] <= arr[right]) {
-            temp.push_back(arr[left]);
-            left++;
-        }
-        else {
-            temp.push_back(arr[right]);
-            right++;
-        }
-    }
-
-    // if elements on the left half are still left //
-
-    while (left <= mid) {
-        temp.push_back(arr[left]);
-        left++;
-    }
-
-    //  if elements on the right half are still left //
-    while (right <= high) {
-        temp.push_back(arr[right]);
-        right++;
-    }
-
-    // transfering all elements from temporary to arr //
-    for (int i = low; i <= high; i++) {
-        arr[i] = temp[i - low];
-    }
+void mergeSort(int a[], int low, int high){
+    if(low>=high) return;
+    int mid = (low+high)/2;
+    mergeSort(a,low,mid);
+    mergeSort(a,mid+1,high);
+    merge(a,low,mid,high);
 }
 
-void mergeSort(vector<int> &arr, int low, int high) {
-    if (low >= high) return;
-    int mid = (low + high) / 2 ;
-    mergeSort(arr, low, mid);  // left half
-    mergeSort(arr, mid + 1, high); // right half
-    merge(arr, low, mid, high);  // merging sorted halves
-}
 
-int main() {
-
-    vector<int> arr = {9, 4, 7, 6, 3, 1, 5}  ;
-    int n = 7;
-
-    cout << "Before Sorting Array: " << endl;
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " "  ;
+int main(){
+    int a[] = {5,4,3,2,1,0};
+    int n = sizeof(a)/sizeof(a[0]);
+    cout<<"Before sorting array -"<<endl;
+    for(int i =0; i<n;i++){
+        cout<<a[i]<<" ";
     }
-    cout << endl;
-    mergeSort(arr, 0, n - 1);
-    cout << "After Sorting Array: " << endl;
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " "  ;
+    cout<<endl;
+    mergeSort(a,0,n-1);
+    cout<<"After sorting array -"<<endl;
+    for(int i =0; i<n;i++){
+        cout<<a[i]<<" ";
     }
-    cout << endl;
-    return 0 ;
+    cout<<endl;
+    return 0;
 }
