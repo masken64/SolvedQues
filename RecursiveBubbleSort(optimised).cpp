32f95c6b4f62156669631510e24588c6c1d497c3
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-06-20 18:20:45 +0530

    Create RecursiveBubbleSort(optimised).cpp

---
+++ RecursiveBubbleSort(optimised).cpp
@@ -0,0 +1,36 @@
+//optimised version of Recursive Bubble Sort by adding ds 
+//O(n)
+#include <iostream>
+
+using namespace std;
+
+void bubbleSort(int a[], int n){
+    if(n==1) return;
+    int ds = 0;
+    for(int i =0; i<=n-2;i++){
+        if(a[i]>a[i+1]){
+            swap(a[i],a[i+1]);
+            ds = 1;
+        }
+    }
+    if(ds == 0) return;
+    bubbleSort(a,n-1);
+}
+
+int main()
+{
+    int a[] = {5,4,3,2,1,0};
+    int n = sizeof(a)/sizeof(a[0]);
+    cout<<"Unsorted - ";
+    for(int i =0; i<n;i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+    bubbleSort(a,n);
+    cout<<"Sorted - ";
+    for(int i =0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+    return 0;
+}
+
