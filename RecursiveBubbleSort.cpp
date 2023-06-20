9d572117721a3a71aae8b5a4b85f2ec0eb6f13cd
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-06-20 17:27:18 +0530

    Create RecursiveBubbleSort.cpp

---
+++ RecursiveBubbleSort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+void bubbleSort(int a[], int n){
+    if(n==1) return; // base condition
+    for(int i =0; i<=n-1;i++){
+        if(a[i]>a[i+1]){
+            swap(a[i],a[i+1]);  // can also use thried variable i.e. temp to swap
+        }
+    }
+    bubbleSort(a,n-1);  // recursive call
+}
+
+int main()
+{
+    int a[] = {5,4,3,2,1,0};
+    int n = sizeof(a)/sizeof(a[0]);
+    cout<<"Unsorted - ";
+    for(int i =0; i<n;i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+    bubbleSort(a,n);
+    cout<<"Sorted - ";
+    for(int i =0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+    return 0;
+}
