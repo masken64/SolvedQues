86e8597a85364d925b460d6759cf4ac4f3b240cd
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-06-22 16:59:31 +0530

    Update Vehicles.cpp

--- Vehicles.cpp
+++ Vehicles.cpp
@@ -1,12 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+// find two wheelers and four wheelers by taking in number of vehiles to be produces i.e. v and number of wheels availaible i.e. w.
 
  
 int main () 
 {
-  int v;
-  int w;
+  int v; // total vehicles
+  int w; // total wheels 
   cin >> v;
   cin >> w;
   int x = ((4*v)-w)/2;
