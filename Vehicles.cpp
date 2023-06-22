0b12cc9cfeb2e1379eb6b4dc48d134ad58c4ca06
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-06-22 16:57:46 +0530

    Create Vehicles.cpp

---
+++ Vehicles.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ 
+int main () 
+{
+  int v;
+  int w;
+  cin >> v;
+  cin >> w;
+  int x = ((4*v)-w)/2;
+  cout<<"TW= "<<x<<" "<<"FW= "<<v-x;
+
+}
