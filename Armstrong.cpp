664e87fad85a35f48b875707965f06370c0975b3
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-06-26 12:06:06 +0530

    Update Armstrong.cpp

--- Armstrong.cpp
+++ Armstrong.cpp
@@ -7,12 +7,12 @@ void checkArm(int b){
 
  
     while(b!=0){
-        b=b%10;
-        sum = sum + b*b*b;
-        b=b/10;
+        b=b%10; //extraction of last digit.
+        sum = sum + b*b*b; // armstrong number check, the number = sum of cubes of the digits.
+        b=b/10; //removing the extracted digit.
     }
     if(b==sum){
-        cout<<"True "<<sum;
+        cout<<"True "<<sum; //checking if arm or not.
     }
     else {
         cout<<"False "<<sum;
@@ -23,8 +23,8 @@ void checkArm(int b){
 int main()
 {
     int a;
-    cout<<"Please enter a number - "<<endl;
-    cin>>a;
+    cout<<"Please enter a number - "<<endl; 
+    cin>>a; // taking input from user.
     checkArm(a);
     
     }
