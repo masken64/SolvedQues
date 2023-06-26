43b3e9b3fd3fc31423a1cc79a40e6f32981cc025
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-06-26 12:11:36 +0530

    Update CheckPallindrome.cpp

--- CheckPallindrome.cpp
+++ CheckPallindrome.cpp
@@ -3,19 +3,19 @@
 using namespace std;
 
 bool Palindrome(int x) {
-        if (x < 0) return false;
+        if (x < 0) return false;  //checking condition if x<0
         long r=0;
-        int y=x;
+        int y=x; //creating new variable so that we can check for pallindrome
         while(y!=0){
-            r=r*10+y%10;
-            y=y/10;
+            r=r*10+y%10;  //extracting last digit and reversing the number
+            y=y/10; //removing last digit
         }
-        return r==x;
+        return r==x;  //bool check
     }
     
 int main(){
     int x=0;
-    cin>>x;
+    cin>>x; //taking input
     cout<<Palindrome(x);
     
 }
