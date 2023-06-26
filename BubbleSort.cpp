205d2619c6e2d67719b07f240421d49dde3b2330
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-06-26 12:10:00 +0530

    Update BubbleSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -1,17 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 void bubbleSort(int a[], int n){
-    for(int i = n-1; i>=0; i--){
-        for(int j =0; j<=i-1;j++){
-            if(a[j]>a[j+1]){
+    for(int i = n-1; i>=0; i--){   //starting from last element.
+        for(int j =0; j<=i-1;j++){   
+            if(a[j]>a[j+1]){       //checking if element at j is greater than element at j+1
                 int temp = a[j];
-                a[j] = a[j+1];
+                a[j] = a[j+1];    //swapping, can also use swap(a[j], a[j+1];
                 a[j+1]= temp;
             }
         }
     }
     cout<<"Sorted Array - "<<endl;
-    for(int i =0;i<n;i++){
+    for(int i =0;i<n;i++){   //printing the sorted array
         cout<<a[i]<<" ";
     }
 }
@@ -19,13 +19,13 @@ int main(){
     int a[5];
     cout<<"Enter Array"<<endl;
     for(int i =0; i<5;i++){
-        cin>>a[i];
+        cin>>a[i]; //user input
     }
     cout<<"Your array before using bubble sort - "<<endl;
     int n = sizeof(a)/sizeof(a[0]);
     for(int i =0; i<n;i++){
-        cout<<a[i]<<" ";
+        cout<<a[i]<<" "; //printing array before using bubblesort
     }
     cout<<endl;
-    bubbleSort(a,n);
+    bubbleSort(a,n); //sorting the array
 }
