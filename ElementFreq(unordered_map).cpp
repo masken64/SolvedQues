a24257977e66e5d7c9e964cce8db4736d807687e
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-06-28 06:04:16 -0700

    Update ElementFreq(unordered_map).cpp

--- ElementFreq(unordered_map).cpp
+++ ElementFreq(unordered_map).cpp
@@ -1,17 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 void freq(int a[], int n){
-  unordered_map<int,int> map;
-  for(int i =0; i<n; i++){
+  unordered_map<int,int> map; 
+  for(int i =0; i<n; i++){ 
     map[a[i]]++;
   }
   for(auto x : map){
-    cout<<x.first<<" "<<x.second<<endl;
+    cout<<x.first<<" "<<x.second<<endl; //outputting frequency of elements.
   }
 }
 int main(){
-  int a[]= { 3,2,2,5,1,6};
-  int n = sizeof(a)/sizeof(a[0]);
+  int a[]= {3,2,2,5,1,6};
+  int n = sizeof(a)/sizeof(a[0]); // used to find length of an array, total size/size of 1 element.
   freq(a,n);
   return 0;
 }
