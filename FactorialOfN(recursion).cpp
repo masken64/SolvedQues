00cd55732487f3ed369841050a0b9b783917277e
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-06-28 06:06:53 -0700

    Update FactorialOfN(recursion).cpp

--- FactorialOfN(recursion).cpp
+++ FactorialOfN(recursion).cpp
@@ -1,15 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 void fac(int a, int f){
-     
-     if(a<1) {
-         
-         cout<<f; return;
-         
-     }
-     
-     fac(a-1,f*a);
+      if(a<1) //if a becomes less than 1 we break the recursion and output the value of factorial.
+      {
+            cout<<f; return;
+         }
+     fac(a-1,f*a); //reducing a and multiplying the value of every iteration with f, which is equal to 1 when initialised. eg, 4! = 4x3x2x1 break and cout f.
 }
+
 int main(){
     int a;
     
