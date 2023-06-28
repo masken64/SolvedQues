9410707e6160ff5d98414e57e4d1c65d6c82407f
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-06-28 06:15:51 -0700

    Update GCD-using-recursion.cpp

--- GCD-using-recursion.cpp
+++ GCD-using-recursion.cpp
@@ -3,8 +3,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 int gcd(int a, int b){
-    if(b==0) return a;
-    return gcd(b,a%b);
+    if(b==0) return a; 
+    return gcd(b,a%b); //eucledian algo by recursion
     
 }
 int main(){
