cbc47f1651fdba08810419a1080acf955bc9bfc4
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-06-29 13:47:07 +0530

    Update SortedCheck.cpp

--- SortedCheck.cpp
+++ SortedCheck.cpp
@@ -5,7 +5,7 @@ using namespace std;
 bool isSorted(int arr[], int n) {
   for (int i = 0; i < n-1; i++)
   // can also use nested for loop insted of usinf i+1; in which j = i+1; j<n-1; j++
-    //arr[j]<arr[i], but will have time complexity of O(N2);
+    //arr[j]<arr[i], but will have time complexity of O(N2)
     
   {
         if (arr[i] > arr[i+1]){
