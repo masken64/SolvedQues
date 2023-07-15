22c1fefa75798cde2f01263efb661ced32ea43ec
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-07-15 12:18:48 +0530

    Update LongestSumarray(+ve and -ve).cpp

--- LongestSumarray(+ve and -ve).cpp
+++ LongestSumarray(+ve and -ve).cpp
@@ -1,3 +1,47 @@
+//brute 
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int getLongestSubarray(vector<int>& a, int k) {
+    int n = a.size(); // size of the array.
+
+    int len = 0;
+    for (int i = 0; i < n; i++) { // starting index
+        for (int j = i; j < n; j++) { // ending index
+            // add all the elements of
+            // subarray = a[i...j]:
+            int s = 0;
+            for (int K = i; K <= j; K++) {
+                s += a[K];
+            }
+
+            if (s == k)
+                len = max(len, j - i + 1);
+        }
+    }
+    return len;
+}
+
+int main()
+{
+    vector<int> a = { -1, 1, 1};
+    int k = 1;
+    int len = getLongestSubarray(a, k);
+    cout << "The length of the longest subarray is: " << len << "\n";
+    return 0;
+}
+
+
+
+
+
+
+
+//optimised 
+
 #include <bits/stdc++.h>
 
 using namespace std;
