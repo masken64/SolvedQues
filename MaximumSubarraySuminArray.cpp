d251655635194f64b5f02659ac2b898025bed0dc
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-07-22 20:38:50 +0530

    Update MaximumSubarraySuminArray.cpp

--- MaximumSubarraySuminArray.cpp
+++ MaximumSubarraySuminArray.cpp
@@ -26,3 +26,30 @@ int main()
 }
 
 
+//optimised 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void compute(int a[], int n ){
+    int maxi = INT_MIN;
+    int sum=0;
+    for(int i =0;i<n;i++){
+        sum = sum+a[i];
+        
+        if(sum>maxi){
+            maxi=sum;
+        }
+        if(sum<0){
+            sum=0;
+        }
+    }
+    cout<<maxi;
+}
+
+int main()
+{
+   int a[] = {-2,1,-3,4,-1,2,1,-5,4};
+   int n = sizeof(a)/sizeof(a[0]);
+   compute(a,n);
+}
