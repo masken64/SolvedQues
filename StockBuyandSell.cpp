8eeb69e50d56587da30954524ccb70f4bd5446c4
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-07-22 22:09:06 +0530

    Update StockBuyandSell.cpp

--- StockBuyandSell.cpp
+++ StockBuyandSell.cpp
@@ -24,3 +24,25 @@ int main()
    int n = sizeof(a)/sizeof(a[0]);
    compute(a,n);
 }
+
+//optimised 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void compute(int a[], int n ){
+    int minP = INT_MAX;
+    int maxP = 0;
+    for(int i =0;i<n;i++){
+        minP = min(minP,a[i]);
+        maxP = max(maxP,a[i]-minP);
+    }
+    cout<<maxP;
+}
+
+int main()
+{
+   int a[] = {7,1,5,3,6,4};
+   int n = sizeof(a)/sizeof(a[0]);
+   compute(a,n);
+}
