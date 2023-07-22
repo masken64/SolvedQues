e89512ca57ec5c0da104c5e6ce1f5585afadf4f8
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-07-22 20:11:33 +0530

    Update ElementOccursMoreThanN-2Times.cpp

--- ElementOccursMoreThanN-2Times.cpp
+++ ElementOccursMoreThanN-2Times.cpp
@@ -27,3 +27,40 @@ int main()
    int n = sizeof(a)/sizeof(a[0]);
    compute(a,n);
 }
+
+
+//optimised (moore's voting algo)
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void compute(int a[], int n ){
+    int cnt =0;
+    int el = a[0];
+    for(int i=0;i<n;i++){
+        if(cnt==0){ cnt =1; el=a[i];}
+        else if(el==a[i]){ cnt++;}
+        else{ cnt--;}
+    }
+    
+    int c1=0;
+    for(int i =0;i<n;i++){
+        if(a[i]==el) {
+            c1++;
+        }
+        
+        if(c1>floor(n/2)){
+            cout<<el;
+            break;
+        }
+    }
+    
+}
+
+int main()
+{
+   int a[] = {2,2,1,1,1,2,2};
+   int n = sizeof(a)/sizeof(a[0]);
+   compute(a,n);
+}
+
