4e6ea930c77cd62f9acd0954dcbee741e3e5589b
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-07-23 16:22:17 +0530

    Create RearrangeArrayElementsBySize(alternate).cpp

---
+++ RearrangeArrayElementsBySize(alternate).cpp
@@ -0,0 +1,35 @@
+//bruteforce 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void compute(int a[], int  n){
+    vector<int> p;
+    vector<int> ne;
+    for(int i =0;i<n;i++){
+        if(a[i]>0){
+            p.push_back(a[i]);
+        }
+        else {
+            ne.push_back(a[i]);
+        }
+    }
+    
+    for(int i =0;i<n/2;i++){
+        a[2*i]=p[i];
+        a[2*i+1]=ne[i];
+    }
+    
+    for(int i =0;i<n;i++){
+        cout<<a[i]<<" ";
+         
+    }
+}
+
+int main()
+{
+int a[] = {1,2,-4,-5};
+int n = sizeof(a)/sizeof(a[0]);
+compute(a,n);
+}
+
