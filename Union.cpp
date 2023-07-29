04fd34a6a834b4be3cd545fbcb29bed5429e005c
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-07-29 19:46:58 +0530

    Update Union.cpp

--- Union.cpp
+++ Union.cpp
@@ -18,7 +18,7 @@ vector < int > FindUnion(int arr1[], int arr2[], int n, int m) {
 int main() {
   int n = 10, m = 7;
   int arr1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  int arr2[] = {2, 3, 4, 4, 5, 11, 12};
+  int arr2[] = {2,3,4,4,5,11,12};
   vector < int > Union = FindUnion(arr1, arr2, n, m);
   cout << "Union of arr1 and arr2 is " << endl;
   for (auto & val: Union)
