e0894b62ee7e1c6e0ee1f17610e96fb90526c3f7
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-07-30 17:04:52 +0530

    Update LongestSequence.cpp

--- LongestSequence.cpp
+++ LongestSequence.cpp
@@ -36,3 +36,38 @@ int main()
     int sol = compute(a,n);
     cout<<sol;
 }
+
+//optimised 
+
+#include <bits/stdc++.h>
+
+using namespace std;
+int compute(vector<int> a, int n){
+    int maxi=1;
+    unordered_set <int> st;
+    for(int i =0;i<n;i++){
+        st.insert(a[i]);
+    }
+    for(auto it : st){
+        if(st.find(it-1)==st.end()){
+            int cn =1;
+            int x = it;
+            while(st.find(x+1)!=st.end()){
+                x++;
+                cn++;
+            }
+            maxi = max(maxi,cn);
+        }
+        
+    }
+    return maxi;
+}
+
+int main()
+{
+    vector<int> a = {0,3,7,2,5,8,4,6,0,1};
+    int n = a.size();
+    int sol = compute(a,n);
+    cout<<sol;
+}
+
