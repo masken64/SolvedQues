668fbb21abef5a1fa61675444324728d1df7f7db
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-07-30 15:07:42 +0530

    Update LeadersInAnArray.cpp

--- LeadersInAnArray.cpp
+++ LeadersInAnArray.cpp
@@ -31,3 +31,32 @@ int main()
     }
 }
 
+//optimised 
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> compute(vector<int> a, int n){
+    vector<int> ans;
+    int max = a[n-1];
+    ans.push_back(a[n-1]);
+    for(int i =n-2;i>=0;i--){
+        if(a[i]>max){
+            ans.push_back(a[i]);
+            max = a[i];
+        }
+    }
+
+    return ans;
+}
+
+int main()
+{
+    vector<int> a = { 10, 22, 12, 3, 0, 6 };
+    int n = a.size();
+    vector<int> sol = compute(a,n);
+    for(auto it : sol){
+        cout<<it<<" ";
+    }
+}
