976efaffce2e035c19830a1fe52dc3b454204b46
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-08-02 17:04:01 +0530

    Update CountSubarraythatEqualstoK.cpp

--- CountSubarraythatEqualstoK.cpp
+++ CountSubarraythatEqualstoK.cpp
@@ -28,3 +28,29 @@ int main(){
     cout<<ans;
 }
 
+
+//optimised 
+#include<bits/stdc++.h>
+using namespace std;
+int compute(vector<int> a, int n, int k){
+    unordered_map<int,int> mpp;
+    int presum = 0;
+    int count = 0;
+    mpp[0]=1;
+    for(int i =0;i<n;i++){
+        presum = presum + a[i];
+        int rem = presum - k;
+        count = count + mpp[rem];
+        mpp[presum]++;
+    }
+    
+    return count;
+}
+int main(){
+    vector<int> a ={3, 1, 2, 4};
+    int n = a.size();
+    int k = 6;
+    int answer = compute(a,n,k);
+    cout<<answer;
+}
+
