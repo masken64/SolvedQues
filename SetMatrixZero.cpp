753cf6ab95b87b13fe492131ea45472e7ffa4054
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-08-02 15:38:18 +0530

    Update SetMatrixZero.cpp

--- SetMatrixZero.cpp
+++ SetMatrixZero.cpp
@@ -53,3 +53,61 @@ vector<vector<int>> sol = compute(a,n,m);
         cout<<endl;
     }
 }
+
+
+//optimised 
+
+#include<bits/stdc++.h>
+using namespace std;
+void compute(vector<vector<int>> &a, int n, int m){
+    int col0 = 1;
+    for(int i =0;i<n;i++){
+        for(int j =0;j<m;j++){
+            if(a[i][j]==0){
+                a[i][0]=0;
+                if(j!=0){
+                    a[0][j]=0;
+                }
+                else{
+                    col0 = 0;
+                }
+            }
+        }
+    }
+    
+    for(int i =1;i<n;i++){
+        for(int j=1;j<m;j++){
+            if(a[i][j]!=0){
+                if(a[0][j]==0 || a[i][0]==0){
+                    a[i][j]=0;
+                }
+            }
+        }
+    }
+    if(a[0][0]==0){
+        for(int j =0;j<m;j++){
+            a[0][j]==0;
+        }
+    }
+    if(col0==0){
+        for(int i =0;i<n;i++){
+            a[i][0]=0;
+        }
+    }
+    
+   
+}
+
+
+int main(){
+    vector<vector<int>> a = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+    int n = a.size();
+    int m = a[0].size();
+    compute(a,n,m);
+    for(auto it : a){
+        for(auto el : it){
+            cout<<el<<" ";
+        }
+        cout<<endl;
+    }
+}
