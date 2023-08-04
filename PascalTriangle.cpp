0aa79752b60682f5472a3df6ecfe161371d54c46
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-08-04 12:56:23 +0530

    Update PascalTriangle.cpp

--- PascalTriangle.cpp
+++ PascalTriangle.cpp
@@ -40,3 +40,38 @@ int main(){
     int n = 5;
     compute(n);
 }
+
+//variation 3, given row n, print pascal triangle upto nth rows
+
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> genR(int row){
+    int ans = 1;
+    vector<int> ansR;
+    ansR.push_back(1);
+    for(int col = 1; col<row;col++){
+        ans = ans*(row-col);
+        ans = ans/col;
+        ansR.push_back(ans);
+    }
+    return ansR;
+}
+
+vector<vector<int>> compute(int n ){
+    vector<vector<int>> ans;
+    for(int row = 1; row<= n; row++){
+        ans.push_back(genR(row));
+    }
+    return ans;
+}
+int main(){
+    int n = 5;
+    vector<vector<int>> ans = compute(n);
+    for(auto it : ans){
+        for(auto el : it){
+            cout<<el<<" ";
+        }
+        cout<<endl;
+    }
+}
