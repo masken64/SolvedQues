4803d0cb1ac8b9e4567fb6a8117ad47f2d770f94
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-08-04 20:38:49 +0530

    Update 3Sum.cpp

--- 3Sum.cpp
+++ 3Sum.cpp
@@ -1,3 +1,32 @@
+//bruteforce 
+class Solution {
+ public:
+  vector<vector<int>> threeSum(vector<int>& a) {
+    set<vector<int>> st;
+    int n = a.size();
+    for(int i =0;i<n;i++){
+        for(int j =i+1;j<n;j++){
+            for(int k=j+1;k<n;k++){
+                if(a[i]+a[j]+a[k]==0){
+                    vector<int> temp = {a[i],a[j],a[k]};
+                    sort(temp.begin(), temp.end());
+                    st.insert(temp);
+                }
+                
+            }
+        }
+    }
+    vector<vector<int>> ans(st.begin(),st.end());
+    return ans;
+  }
+};
+
+
+
+
+
+
+
 //optimied 
 #include<bits/stdc++.h>
 using namespace std;
