626902e3c1c76f17db407038bc033c74cdf10325
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-08-13 18:45:50 +0530

    Update SubarrayWithGivenXorK.cpp

---
+++ SubarrayWithGivenXorK.cpp
@@ -0,0 +1,37 @@
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int subarraysWithXorK(vector<int> a, int k) {
+    int n = a.size(); //size of the given array.
+    int cnt = 0;
+
+    // Step 1: Generating subarrays:
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; j++) {
+
+            //step 2:calculate XOR of all
+            // elements:
+            int xorr = 0;
+            for (int K = i; K <= j; K++) {
+                xorr = xorr ^ a[K];
+            }
+
+            // step 3:check XOR and count:
+            if (xorr == k) cnt++;
+        }
+    }
+    return cnt;
+}
+
+int main()
+{
+    vector<int> a = {4, 2, 2, 6, 4};
+    int k = 6;
+    int ans = subarraysWithXorK(a, k);
+    cout << "The number of subarrays with XOR k is: "
+         << ans << "\n";
+    return 0;
+}
