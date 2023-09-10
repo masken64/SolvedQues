36ffef03e020c90a560c97e8c60afd16d3f5f7e9
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-09-10 16:02:59 +0530

    Update IBM_prep.cpp

--- IBM_prep.cpp
+++ IBM_prep.cpp
@@ -78,3 +78,32 @@ int main(){
 }
 
 ************************************************************************
+//Write a program to generate Fibonacci Triangle
+
+#include<bits/stdc++.h>
+using namespace std;
+void fib(int f[], int N){
+    f[1]=1;
+    f[2]=1;
+    for(int i =3; i<=N;i++){
+        f[i]= f[i-1]+f[i-2];
+    }
+}
+void fibTriangle(int n){
+    int N = n*(n+1)/2;
+    int f[N+1];
+    fib(f,N);
+    int count =1;
+    for(int i =1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<f[count++]<<" ";
+            
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    fibTriangle(n);
+}
